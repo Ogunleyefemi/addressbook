83ede2e95266f7aeaa190537bbfb12c20812d7ec
femi <oluwafemiogunleye21@gmail.com>
2022-11-07 02:30:14 -0800

    add constructor amd prototypes

---
+++ app.js
@@ -0,0 +1,25 @@
+// constructor and prototypes
+function Addressbook(firstName,lastName,phoneNumber,email,address) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.address = address;
+}
+// constructor
+let User = new Addressbook('femi','OG',09012345678,'fm@yahoo.ng','No 2,bola way lagos');
+Addressbook.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName
+}
+Addressbook.prototype.Mobile = function(){
+    return '+234' + (this.phoneNumber)
+};
+
+// user2
+let User2 = new Addressbook('Emmanuel', 'James',08012345678,'emmy@yahoo.mail','ahamadu bello way');
+Addressbook.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName
+}
+Addressbook.prototype.Mobile = function(){
+    return '+234' + (this.phoneNumber)
+};
